d56bdbfca0788961a82fb690bf949c561562ef8a
Tuyizere Esther <tuyizereesther@akilahinstitute.org>
2019-10-04 03:44:19 -0700

    ft(challenge2):create or add new article[#168942096]

--- server/routes/route-path.js
+++ server/routes/route-path.js
@@ -1,9 +1,11 @@
 import  express from 'express';
 import Users from '../controller/User';
+import Articles from '../controller/Articles';
 
 const router = express();
 
 router.post('/api/v1/auth/userSignup/', Users.addUser);
 router.post('/api/v1/auth/signin', Users.login);
+router.post('/api/v1/articles', Articles.addArticle);
 
 export default router
