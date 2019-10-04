b3c742fdd56e47b74f3e3627f872f8c677274342
Tuyizere Esther <tuyizereesther@akilahinstitute.org>
2019-10-04 01:16:15 -0700

    ft(challenge):user login[#168939832]

--- server/routes/route-path.js
+++ server/routes/route-path.js
@@ -4,5 +4,6 @@ import Users from '../controller/User';
 const router = express();
 
 router.post('/api/v1/auth/userSignup/', Users.addUser);
+router.post('/api/v1/auth/signin', Users.login);
 
 export default router
--- server/test.js
+++ server/test.js
@@ -21,4 +21,4 @@ describe('/POST Create user', () => {
             });
         });
     });
-
+    
